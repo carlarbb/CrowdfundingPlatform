5cd62080b326ddc06e365706f075203cd634d9a0
ursuadina <ursuadina98@yahoo.com>
2020-06-08 13:02:24 +0300

    Added Spring profiles

---
+++ src/main/java/com/example/demo/configuration/SystemProfileValueSource2.java
@@ -0,0 +1,24 @@
+package com.example.demo.configuration;
+
+import org.springframework.stereotype.Component;
+import org.springframework.test.annotation.ProfileValueSource;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+
+import java.util.Objects;
+
+@Component
+public class SystemProfileValueSource2 implements ProfileValueSource {
+
+    @Override
+    public String get(String key) {
+        Assert.hasText(key, "'key' must not be empty");
+        String springProfiles = System.getProperty(key);
+        if (Objects.isNull(key) || StringUtils.isEmpty(springProfiles)) {
+            return "mongo-embedded";
+        }
+
+        return System.getProperty(key);
+    }
+
+}
